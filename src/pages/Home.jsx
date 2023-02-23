a9394ef75142da49dacc0353dbe996c80e8b0fe0
C-Lee-Hamilton <122707378+C-Lee-Hamilton@users.noreply.github.com>
2023-02-23 12:58:01 -0600

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { List } from '../components/List';
+
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Look at all those Chickéns </h1>
+      <List/>
+      
+    
+    </div>
+  )
+}
